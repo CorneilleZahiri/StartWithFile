ad7d08c05c19f2a1c9c372afe1ff10dcc6a69700
Corneille <cgbeaga@gmail.com>
2025-05-12 19:55:24 +0100

    Amélioration du code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,44 +10,50 @@ public class Main {
         creerEtEcrireDansUnFichier();
         LireUnFichier();
     }
-    static void creerEtEcrireDansUnFichier(){
+
+    static void creerEtEcrireDansUnFichier() {
         Scanner scanner = new Scanner(System.in);
 
-        try{
-            File monFichier = new File("notes.txt");
-
-            if (monFichier.exists()) monFichier.delete();
-
-            if (monFichier.createNewFile()){
-                Path path = Paths.get("notes.txt");
-
-                System.out.print("*******************************" +
-                        "\nEntrez 3 phrases dans le fichiers.\n1- ");
-                String phrase1 = scanner.nextLine();
-                System.out.print("2- ");
-                String phrase2 = scanner.nextLine();
-                System.out.print("3- ");
-                String phrase3 = scanner.nextLine();
-
-                //AJOUTER LES PHRASES DANS LE FICHIER
-                Files.write(path, List.of(phrase1, phrase2, phrase3));
-
-                System.out.println("Fichier notes.txt crée avec succès");
+        try {
+            Path path = Paths.get("notes.txt");
+            if (Files.exists(path)) {
+                Files.delete(path);
+                System.out.println("Fichier existant supprimé avec succès");
             }
-        }catch (IOException e){
+
+            Files.createFile(path);
+
+            System.out.print("*******************************" +
+                    "\nEntrez 3 phrases dans le fichiers.\n1- ");
+            String phrase1 = scanner.nextLine();
+            System.out.print("2- ");
+            String phrase2 = scanner.nextLine();
+            System.out.print("3- ");
+            String phrase3 = scanner.nextLine();
+
+            //AJOUTER LES PHRASES DANS LE FICHIER
+            Files.write(path, List.of(phrase1, phrase2, phrase3));
+
+            System.out.println("Fichier notes.txt crée avec succès");
+
+        } catch (IOException e) {
             System.err.println("Erreur lors de la création du fichier");
             e.printStackTrace();
         }
     }
 
-    static void LireUnFichier(){
+    static void LireUnFichier() {
         Path path = Paths.get("notes.txt");
-        try{
+        try {
             List<String> contenuFichier = Files.readAllLines(path);
-            System.out.println("Contenu du fichier ligne par ligne : ");
-            System.out.println(contenuFichier);
+            
+            System.out.println("*********** DEBUT DE LECTURE **************");
+            for (String ligne : contenuFichier) {
+                System.out.println(ligne);
+            }
+            System.out.println("*********** FIN DE LECTURE **************");
 
-        }catch (IOException e){
+        } catch (IOException e) {
             System.err.println("Erreur de lecture du fichier");
             e.printStackTrace();
         }
